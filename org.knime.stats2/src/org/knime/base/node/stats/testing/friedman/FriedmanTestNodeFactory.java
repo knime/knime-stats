4af139dfc2f97e80b077f19ee39ac3ebb0c1a9b4
Manuel Hotz <manuel.hotz@knime.com>
2026-01-27 10:30:14 +0100

    UIEXT-3119: Fix title case and stepSize and remove superflous persistors
    UIEXT-3119 (WebUI-Migration Friedman Test)

--- org.knime.stats2/src/org/knime/base/node/stats/testing/friedman/FriedmanTestNodeFactory.java
+++ org.knime.stats2/src/org/knime/base/node/stats/testing/friedman/FriedmanTestNodeFactory.java
@@ -45,6 +45,12 @@
  */
 package org.knime.base.node.stats.testing.friedman;
 
+import static org.knime.node.impl.description.PortDescription.fixedPort;
+
+import java.util.List;
+import java.util.Map;
+
+import org.knime.core.node.NodeDescription;
 import org.knime.core.node.NodeDialogPane;
 import org.knime.core.node.NodeFactory;
 import org.knime.core.node.NodeView;
@@ -52,17 +58,12 @@ import org.knime.core.webui.node.dialog.NodeDialog;
 import org.knime.core.webui.node.dialog.NodeDialogFactory;
 import org.knime.core.webui.node.dialog.NodeDialogManager;
 import org.knime.core.webui.node.dialog.SettingsType;
-import org.knime.core.webui.node.dialog.defaultdialog.DefaultNodeDialog;
 import org.knime.core.webui.node.dialog.defaultdialog.DefaultKaiNodeInterface;
-import org.knime.core.node.NodeFactory.NodeType;
+import org.knime.core.webui.node.dialog.defaultdialog.DefaultNodeDialog;
 import org.knime.core.webui.node.dialog.kai.KaiNodeInterface;
 import org.knime.core.webui.node.dialog.kai.KaiNodeInterfaceFactory;
-import org.knime.core.node.NodeDescription;
 import org.knime.node.impl.description.DefaultNodeDescriptionUtil;
-import java.util.Map;
 import org.knime.node.impl.description.PortDescription;
-import java.util.List;
-import static org.knime.node.impl.description.PortDescription.fixedPort;
 
 /**
  * <code>NodeFactory</code> for the "FriedmanTest" Node.
@@ -117,12 +118,12 @@ extends NodeFactory<FriedmanTestNodeModel> implements NodeDialogFactory, KaiNode
             """;
     private static final List<PortDescription> INPUT_PORTS = List.of(
             fixedPort("Input data", """
-                The table from which to test samples
+                The table from which to test samples.
                 """)
     );
     private static final List<PortDescription> OUTPUT_PORTS = List.of(
             fixedPort("Evaluation", """
-                Friedman test evaluation
+                Friedman test evaluation.
                 """)
     );
 
